5f3b9bd34c4fd834da3bdee923837e4dcf58647b
Jason B <j.bury@hotmail.fr>
2015-11-27 11:52:54 +0100

    The structure of Graph
    I made the structure of one class to test make. It doesn't need to add
    the name of the file Graph in cmakelist.

---
+++ src/Graph.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * The graph. Initial data of the problem
+ */
+ #include <vector>
+ #include <string>
+ 
+class Graph {
+	private:
+		/** The name of the file that contain data of the problem **/
+		std::string filename;
+		/** head[i] is the index of the first successor of i. So the list of links **/
+		int head[];
+		/** Contain successors of nodes (**/
+		int successors[];
+		/** Nflyers[i] is the amount of flyers in the link i **/
+		int Nflyers[];
+		/** The x coordinate of nodes **/
+		int x[];
+		/** The y coordinate of nodes **/
+		int y[];
+	public:
+		/**
+		 * @param the name of the file that contain all data
+		 */
+		Graph(std::string name){
+			filename = name;
+		}
+		/**
+		 * @return the first successor of 'node'
+		 */
+		int getFirst(int node){
+			//TODO
+		}
+		/**
+		 * @return the number of successor of 'node'
+		 */
+		int getCount(int node){//utile pour getSuccessors
+			//TODO
+		}
+		/**
+		 * @return All successors of 'node'
+		 */
+		std::vector<int> getSuccessors(int node){
+			//TODO
+		}
+};
--- src/metacombi.cpp
+++ src/metacombi.cpp
@@ -4,6 +4,7 @@
  */
 #include <iostream>
 #include <string>
+#include "Graph.cpp"
 
 /**
  * Some doc for a simple class.
