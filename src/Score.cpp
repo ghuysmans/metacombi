1d475678f9b99e7cd8eed5a3713965dc18c22b1e
Guillaume Huysmans <ghuysmans99@gmail.com>
2015-12-03 00:32:42 +0100

    Initializer list instead of assignment

--- src/Score.cpp
+++ src/Score.cpp
@@ -1,11 +1,9 @@
 #include <vector>
 #include "Score.h"
 
-Score::Score(std::vector<struct teamScore> teamScoreList){
-	tsList = teamScoreList;
+Score::Score(std::vector<struct teamScore> teamScoreList): tsList(teamScoreList){
 }
 
-bool Score::operator<(const Score right){
-	/* the element on the left of the operator is 'this' and its a pointer.*/
+bool Score::operator<(const Score right){ //this compares "this" and "right"
 	//TODO
 }
