cc4d793c8b25473da0e41838e62be5b290ec9511
Locqueneux Semih <semih.locqueneux@hotmail.fr>
2015-12-03 20:39:38 +0100

    unit test for solution added(still a failure )

---
+++ tests/USolution.cpp
@@ -0,0 +1,16 @@
+#include "Graph.h"
+#include "Solution.h"
+#include "CuTest.h"
+#include <iostream>
+
+void TestIsAdmissible(CuTest *tc) {
+    /*std::cout << "before opening" << std::endl;
+	Graph graph = Graph("TPOC-45.txt");
+	static const int arr[] = {1,2,1,1,2,1,1,2,1,1,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,3,3,2,3,1,3,2,1,1,3,1,1,3,3,3,3,3,3,3,3,3,3,3};
+	std::vector<int> vectorSolution(sizeof(arr) / sizeof(arr[0]), 0);
+    std::cout << "before constructor" << std::endl;
+	Solution sol = Solution(vectorSolution,graph);
+    std::cout << "still alive" << std::endl;
+	CuAssertTrue(tc, sol.isAdmissible());
+    std::cout << "nope" << std::endl;*/
+}
