4b59cd022702b71cf70378bb029c3c18e144d859
Jason B <j.bury@hotmail.fr>
2015-12-12 18:00:56 +0100

    Inserted in comment one test about comparison

--- tests/USolution.cpp
+++ tests/USolution.cpp
@@ -3,6 +3,7 @@
 #include "CuTest.h"
 #include <iostream>
 #include <vector>
+#include "Score.h"
 
 void TestIsAdmissible(CuTest *tc) {
 	Graph graph = Graph::load("testing-Graph.txt");
@@ -32,3 +33,30 @@ void TestCompacitySimple(CuTest *tc){
 	int result = s.getCompacity( g );
 	CuAssertIntEquals(tc, 1350, result);
 }
+
+/* unknown error void Testscore1(CuTest *tc){
+	struct Score::teamScore ts1 = { .dist=250, .Nflyers=280, .comp=80};
+	struct Score::teamScore ts2 = { .dist=265, .Nflyers=280, .comp=80};
+	struct Score::teamScore ts3 = { .dist=240, .Nflyers=280, .comp=80};
+	struct Score::teamScore ts4 = { .dist=245, .Nflyers=280, .comp=80};
+	std::vector<struct Score::teamScore> list1 = std::vector<struct Score::teamScore>(4);
+	list1[0] = ts1;
+	list1[1] = ts2;
+	list1[2] = ts3;
+	list1[3] = ts4;
+	Score::teamScore ts5 = { .dist=280, .Nflyers=280, .comp=80};
+	Score::teamScore ts6 = { .dist=240, .Nflyers=280, .comp=80};
+	Score::teamScore ts7 = { .dist=235, .Nflyers=280, .comp=80};
+	Score::teamScore ts8 = { .dist=245, .Nflyers=280, .comp=80};
+	std::vector<struct Score::teamScore> list2 = std::vector<Score::teamScore>(4);
+	list2[0] = ts5;
+	list2[1] = ts6;
+	list2[2] = ts7;
+	list2[3] = ts8;
+	int average;
+	for(int i=0 ; i<list1.size() ; i++) average += list1.at(i).dist;
+	average = average/list1.size();
+	Score score1 = Score(list1 , average , 280);
+	Score score2 = Score(list2 , average , 280);
+	CuAssertTrue(tc, score2<score1);
+}*/
