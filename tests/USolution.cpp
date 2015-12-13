e53ddf097cd6dc10fe41bbe3efdd647a196a01f2
Jason B <j.bury@hotmail.fr>
2015-12-13 17:40:15 +0100

    Started a test but doesn't compile

--- tests/USolution.cpp
+++ tests/USolution.cpp
@@ -50,7 +50,7 @@ void TestCompacitySimple(CuTest *tc){
 	CuAssertIntEquals(tc, 1350, result);
 }
 
-void TestCompacityBig(CuTest *tc){
+void TestCompacityBig(CuTest *tc){//TODO
 	Graph graph = Graph::load("testing-Graph.txt");
 	int arr[] = {1,2,1,1,2,1,1,2,1,1,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,3,3,2,3,1,3,2,1,1,3,1,1,3,3,3,3,3,3,3,3,3,3,3};
 	std::vector<int> vectorSolution(&arr[0], &arr[0]+48);
@@ -59,29 +59,11 @@ void TestCompacityBig(CuTest *tc){
 }
 	
 
-/* unknown error void Testscore1(CuTest *tc){
-	struct Score::teamScore ts1 = { .dist=250, .Nflyers=280, .comp=80};
-	struct Score::teamScore ts2 = { .dist=265, .Nflyers=280, .comp=80};
-	struct Score::teamScore ts3 = { .dist=240, .Nflyers=280, .comp=80};
-	struct Score::teamScore ts4 = { .dist=245, .Nflyers=280, .comp=80};
-	std::vector<struct Score::teamScore> list1 = std::vector<struct Score::teamScore>(4);
-	list1[0] = ts1;
-	list1[1] = ts2;
-	list1[2] = ts3;
-	list1[3] = ts4;
-	Score::teamScore ts5 = { .dist=280, .Nflyers=280, .comp=80};
-	Score::teamScore ts6 = { .dist=240, .Nflyers=280, .comp=80};
-	Score::teamScore ts7 = { .dist=235, .Nflyers=280, .comp=80};
-	Score::teamScore ts8 = { .dist=245, .Nflyers=280, .comp=80};
-	std::vector<struct Score::teamScore> list2 = std::vector<Score::teamScore>(4);
-	list2[0] = ts5;
-	list2[1] = ts6;
-	list2[2] = ts7;
-	list2[3] = ts8;
-	int average;
-	for(int i=0 ; i<list1.size() ; i++) average += list1.at(i).dist;
-	average = average/list1.size();
-	Score score1 = Score(list1 , average , 280);
-	Score score2 = Score(list2 , average , 280);
-	CuAssertTrue(tc, score2<score1);
-}*/
+void Testscore1(CuTest *tc){
+	std::vector<int> v = std::vector<int>(14, 0);
+	Graph g = Graph::load("scoreSimpleTest.txt");
+	Solution s = Solution(v, g);
+	std::vector<int> v2 = std::vector<int>(14, 1);
+	Solution s2 = Solution(v2, g);
+	CuAssertTrue(tc, !( Score(s2)<Score(s)) );
+}
